5bfac947c621e034757cd4a62a0e8bfc048a1072
PebbleDevelopment <mnashesduna@gmail.com>
2025-04-04 19:58:51 +0200

    initialise app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen w-full flex items-center justify-center bg-gray-100'>
+       here we go again
+       </div>
+    </>
+  )
+}
+
+export default App
